93a3a00331dc56262a948d87848e62fcaa7eb0c0
Peter Lai <milk4candy@gmail.com>
2017-09-15 11:56:43 +0800

    tewwk

--- miner/xmrig-cpu/run_xmrig-cpu_container.sh
+++ miner/xmrig-cpu/run_xmrig-cpu_container.sh
@@ -1,6 +1,5 @@
-docker run --name xmrig-cpu -td --restart unless-stopped --read-only -c 756 bitnn/alpine-xmrig \
+docker run --name xmrig-cpu -dt --restart unless-stopped --read-only bitnn/alpine-xmrig \
     -o xmr-asia1.nanopool.org:14444 \
     --donate-level=1 \
-    -t 3 \
     -p x \
-    -u 48YzKUCcNV4MCaaHmFgJcc9SSWqBPVBEzic3ifoavqU6W8Ue3qSmNfXQhNvuVx2ZFQCLmwVa33483Kxwv4emG3kN69WdXXz.vicky/milk4candy
+    -u 48YzKUCcNV4MCaaHmFgJcc9SSWqBPVBEzic3ifoavqU6W8Ue3qSmNfXQhNvuVx2ZFQCLmwVa33483Kxwv4emG3kN69WdXXz.${HOSTNAME}/milk4candy
