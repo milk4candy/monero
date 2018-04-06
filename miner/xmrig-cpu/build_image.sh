ab7c5be64dbf56088a6eba3f9176477990c1cb38
Peter Lai <milk4candy@gmail.com>
2018-04-06 17:38:00 +0800

    upgrade miner to v2.5.2 for hard fork

---
+++ miner/xmrig-cpu/build_image.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+docker build -t milk4candy/xmrig:v2.5.2 .
--- miner/xmrig-cpu/run_xmrig-cpu_container.sh
+++ miner/xmrig-cpu/run_xmrig-cpu_container.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-docker run --name xmrig-cpu -dt --restart unless-stopped --read-only bitnn/alpine-xmrig \
-    -o xmr-asia1.nanopool.org:14444 \
+docker run --name xmrig-cpu -dt --restart unless-stopped --read-only milk4candy/xmrig:v2.5.2 \
+    -o 54.95.171.245:14444 \
     --donate-level=1 \
     -p x \
     -u 48YzKUCcNV4MCaaHmFgJcc9SSWqBPVBEzic3ifoavqU6W8Ue3qSmNfXQhNvuVx2ZFQCLmwVa33483Kxwv4emG3kN69WdXXz.${HOSTNAME}/milk4candy@gmail.com
